99ac6ad1ca47c87eb50cc77cffe348128fbaac78
Pablo Lopes <pabloluisslopes@gmail.com>
2022-12-12 11:00:02 -0300

    tarefaphp

---
+++ lojaweb-vue/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavBar from '../components/NavBar.vue';
+// export default {
+//   components: { NavBar }
+// }
+</script>
+<template>
+  <NavBar></NavBar>
+  <section>
+    <h1>This is an about page</h1>
+  </section>
+</template>
+
+<style scoped>
+</style>
---
+++ lojaweb-vue/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from '../components/NavBar.vue';
+// export default {
+//   components: { NavBar }
+// }
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <section>
+    <h2>Home</h2>
+  </section>
+</template>
