b334a4467ec96389dd0a6f868bf31e701cd2755d
Pablo Lopes <pabloluisslopes@gmail.com>
2022-12-17 22:23:34 -0300

    atualizacao

--- api/model/Endereco.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Endereco {        
-        public $cep;//PK - primary key (string)
-        public $logradouro;
-        public $bairro;
-        public $cidade;
-        public $uf;
-    }
-?>
--- api/model/Funcionario.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "Usuario.php";
-    class Funcionario extends Usuario{
-        public $id_funcionario;//PK - primary key (int)
-        public $cargo;
-    }
-?>
